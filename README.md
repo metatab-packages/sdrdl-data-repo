464257ce083b92e6e9323da03d8d808f1a9c97c9
Eric Busboom <eric@civicknowledge.com>
2019-05-15 15:26:16 -0700

    moving

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Scripts and lists related to the old version of data.sandiegodata.org
 Generate the datasets.jsonl file with:
 
      $ ckanapi dump datasets --all -O datasets.jsonl.gz -z -p 4 -r https://data.sandiegodata.org
+     
+     
+Submit all of the packages to WP:
+
+    $ cd non-mt-packages
+    $ for i in `ls`; do mp wp -s testwp -p  $i; done
--- make-datapackages.py
+++ make-datapackages.py
@@ -1,6 +1,6 @@
 
 import json
-from metapack import MetapackDoc
+from metapack import MetapackDoc, open_package
 from os import getenv
 from os.path import expanduser
 from metapack.util import datetime_now
@@ -17,6 +17,7 @@ def make_package(md):
     name = md['name']
 
     root = Path('packages').joinpath(name)
+
 
     root.mkdir(parents=True, exist_ok=True)
 
@@ -47,7 +48,29 @@ def make_package(md):
     doc['Root'].find_first('Root.Title').value = md['title']
     
     doc.write_csv(str(root.joinpath('metadata.csv')))
+    
+    return doc
 
+def update_package(md):
+    
+    name = md['name']
+
+    root = Path('packages').joinpath(name)
+
+    if not root.exists():
+        return 
+        
+    doc = MetapackDoc(str(root.joinpath('metadata.csv')))
+    
+    print(doc.name, doc.ref)
+
+    for t in md['tags']:
+        t = doc['Root'].new_term('Root.Tag', t['name'])
+        print(t)
+
+    #print(json.dumps(md, indent=4))
+
+    doc.write()
 
 with Path('datasets.jsonl').open() as f:
      for l in f:
@@ -58,6 +81,6 @@ with Path('datasets.jsonl').open() as f:
          name = ex.get('root.name', ex.get('name',''))
         
          if not name:
-             make_package(md)
+             update_package(md)
         
     
